f91772c0a4e4a7e115611775c9c081387ecc1074
urmybestfriend <c.goldentiger.c@gmail.com>
2016-03-04 13:43:42 +0300

    Fix eslint warnings and refactor code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,33 +5,34 @@ import cn from 'classnames';
 
 export default class Footer extends PureComponent {
 
-  static propTypes = {
-    activeFilter: PropTypes.string.isRequired,
-    dispatch: PropTypes.func.isRequired,
-  }
+    static propTypes = {
+        activeFilter: PropTypes.string.isRequired,
+        dispatch: PropTypes.func.isRequired,
+    }
 
-  filters = ['all', 'completed', 'active']
+    filters = ['all', 'completed', 'active']
 
-  render() {
-    const { dispatch } = this.props;
-    return (
-      <div>
-        <div className="btn-group">
-          {this.filters.map(filter => {
-            const className = cn('btn btn-default capitalize', {
-              active: this.props.activeFilter === filter,
-            });
-            return (
-              <button key={filter} className={className} onClick={() => dispatch(changeFilter(filter))}>
-                {filter}
-              </button>
-            );
-          })}
-        </div>
-        <div className="pull-right">
-          <button className="btn btn-danger" onClick={() => dispatch(deleteAllTodos())}>Delete all</button>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const { dispatch } = this.props;
+        return (
+            <div>
+                <div className="btn-group">
+                    {this.filters.map(filter => {
+                        const className = cn('btn btn-default capitalize', {
+                            active: this.props.activeFilter === filter,
+                        });
+                        return (
+                            <button key={filter} className={className} onClick={() => dispatch(changeFilter(filter))}>
+                                {filter}
+                            </button>
+                        );
+                    })}
+                </div>
+                <div className="pull-right">
+                    <button className="btn btn-danger" onClick={() => dispatch(deleteAllTodos())}>Delete all</button>
+                </div>
+            </div>
+        );
+    }
 }
+
