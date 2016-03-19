3aa1893167dc5f8fbd8d8cac328f1f5218de677d
urmybestfriend <c.goldentiger.c@gmail.com>
2016-03-19 12:30:44 +0300

    refactor generator

--- init_data/computers.js
+++ init_data/computers.js
@@ -1,72 +1,24 @@
 'use strict';
-var getRandomInt = require('./getRandomInt');
+
 var generator = require('./generator');
 
 var computerModel = {
-    mark: function () {
-        var arr = ['ASUS', 'Lenovo', 'HP', 'Apple', 'MSI', 'Acer', 'Dell', 'Toshiba', 'Sony', 'DEXP'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    model: function () {
-        var arr = ['GX-700', '6QE-053', '9F4-5', 'DM036T', 'Zenbook Pro', 'GL552VW-DM321T', 'EeeBook', 'Surface', 'Redhost', 'MEDIA CENTER 1574', 'Mac mini', 'Mac Pro', 'Z800', 'IdeaPad', 'y580', 'optima', 'GAMER 1748'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    color: function () {
-        var arr = ['black', 'white', 'green', 'grey', 'yellow', 'blue', 'red', 'orange', 'violet', 'purple'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    wifi: function () {
-        var arr = ['802.11ac', '802.11a', '802.11c'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    isLaptop: function () {
-        var arr = [true, false];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    diagonal: function () {
-        var arr = [11.1, 11.3, 13.3, 13.7, 14.0, 15.6, 17.1, 17.3, 18.6, 20.1];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    coresNumber: function () {
-        var arr = [1, 2, 4, 8, 16];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    usb2: function () {
-        var arr = [0, 1, 2, 3];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    usb3: function () {
-        var arr = [0, 1, 2, 3, 4];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    ram: function () {
-        var arr = [1, 2, 4, 8, 16, 32, 64];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    memory: function () {
-        var arr = [128, 256, 512, 1024, 2048, 4096, 8192];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    videocard: function () {
-        var arr = ['Intel HD Graphics 3000', 'NVidia GeForce GTX 640', 'Intel HD Graphics 4000', 'NVidia GeForce GTX 730', 'NVidia GeForce GTX 950'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    videomemory: function () {
-        var arr = [1, 2, 4, 8, 16, 32, 64];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    processor: function () {
-        var arr = ['Intel', 'AMD', 'Mac'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    operatingSystem: function () {
-        var arr = ['Windows', 'Linux', 'Mac OS X'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    price: function () {
-        var arr = [300, 340, 380, 420, 460, 495, 510, 530, 560, 580, 640, 670, 725, 760, 780, 840, 850, 860, 920, 940, 1020, 1150, 1280, 1500];
-        return arr[getRandomInt(0, arr.length - 1)];
-    }
+    mark: ['ASUS', 'Lenovo', 'HP', 'Apple', 'MSI', 'Acer', 'Dell', 'Toshiba', 'Sony', 'DEXP'],
+    model: ['GX-700', '6QE-053', '9F4-5', 'DM036T', 'Zenbook Pro', 'GL552VW-DM321T', 'EeeBook', 'Surface', 'Redhost', 'MEDIA CENTER 1574', 'Mac mini', 'Mac Pro', 'Z800', 'IdeaPad', 'y580', 'optima', 'GAMER 1748'],
+    color: ['black', 'white', 'green', 'grey', 'yellow', 'blue', 'red', 'orange', 'violet', 'purple'],
+    wifi: ['802.11ac', '802.11a', '802.11c'],
+    isLaptop: [true, false],
+    diagonal: [11.1, 11.3, 13.3, 13.7, 14.0, 15.6, 17.1, 17.3, 18.6, 20.1],
+    coresNumber: [1, 2, 4, 8, 16],
+    usb2: [0, 1, 2, 3],
+    usb3: [0, 1, 2, 3, 4],
+    ram: [1, 2, 4, 8, 16, 32, 64],
+    memory: [128, 256, 512, 1024, 2048, 4096, 8192],
+    videocard: ['Intel HD Graphics 3000', 'NVidia GeForce GTX 640', 'Intel HD Graphics 4000', 'NVidia GeForce GTX 730', 'NVidia GeForce GTX 950'],
+    videomemory: [1, 2, 4, 8, 16, 32, 64],
+    processor: ['Intel', 'AMD', 'Mac'],
+    operatingSystem: ['Windows', 'Linux', 'Mac OS X'],
+    price: [300, 340, 380, 420, 460, 495, 510, 530, 560, 580, 640, 670, 725, 760, 780, 840, 850, 860, 920, 940, 1020, 1150, 1280, 1500]
 };
 
 module.exports = generator(computerModel, 200);
--- init_data/phones.js
+++ init_data/phones.js
@@ -1,56 +1,20 @@
 'use strict';
-var getRandomInt = require('./getRandomInt');
+
 var generator = require('./generator');
 
 var phoneModel = {
-    mark: function () {
-        var arr = ['iPhone', 'Samsung', 'HTC', 'Huawey', 'Lenovo', 'Sony', 'Nokia', 'Xaomi', 'LG', 'ZTE'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    model: function () {
-        var arr = ['6s', '5s', 'Galaxy', 'P70-A', 'Light Dual', 'Vibe', 'P1M', '650', 'Grand Prime', 'MX5'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    color: function () {
-        var arr = ['black', 'white', 'green', 'grey', 'yellow', 'blue', 'red', 'orange', 'violet', 'purple'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    wifi: function () {
-        var arr = ['802.11ac', '802.11a', '802.11c'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    gps: function () {
-        var arr = [true, false];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    coresNumber: function () {
-        var arr = [1, 2, 4, 8];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    ram: function () {
-        var arr = [1, 2, 4, 8];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    memory: function () {
-        var arr = [2, 4, 8, 16, 32, 64];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    camera: function () {
-        var arr = [5, 8, 10, 12, 13, 15, 18];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    diagonal: function () {
-        var arr = [4, 4.1, 4.2, 4.4, 4.5, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.5, 5.7, 6.0, 6.1, 6.3];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    operatingSystem: function () {
-        var arr = ['Android', 'iOS', 'Window Phone'];
-        return arr[getRandomInt(0, arr.length - 1)];
-    },
-    price: function () {
-        var arr = [200, 220, 240, 260, 280, 300, 330, 360, 390, 420, 450, 480, 500, 540, 560, 580, 600, 620, 660, 710, 730, 800];
-        return arr[getRandomInt(0, arr.length - 1)];
-    }
+    mark: ['iPhone', 'Samsung', 'HTC', 'Huawey', 'Lenovo', 'Sony', 'Nokia', 'Xaomi', 'LG', 'ZTE'],
+    model: ['6s', '5s', 'Galaxy', 'P70-A', 'Light Dual', 'Vibe', 'P1M', '650', 'Grand Prime', 'MX5'],
+    color: ['black', 'white', 'green', 'grey', 'yellow', 'blue', 'red', 'orange', 'violet', 'purple'],
+    wifi: ['802.11ac', '802.11a', '802.11c'],
+    gps: [true, false],
+    coresNumber: [1, 2, 4, 8],
+    ram: [1, 2, 4, 8],
+    memory: [2, 4, 8, 16, 32, 64],
+    camera: [5, 8, 10, 12, 13, 15, 18],
+    diagonal: [4, 4.1, 4.2, 4.4, 4.5, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.5, 5.7, 6.0, 6.1, 6.3],
+    operatingSystem: ['Android', 'iOS', 'Window Phone'],
+    price: [200, 220, 240, 260, 280, 300, 330, 360, 390, 420, 450, 480, 500, 540, 560, 580, 600, 620, 660, 710, 730, 800]
 };
 
 module.exports = generator(phoneModel, 250);
