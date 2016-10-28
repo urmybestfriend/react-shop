2e40b01b81bf58f3a2df5747aa6df735a37cdbfb
urmybestfriend <c.goldentiger.c@gmail.com>
2016-10-28 11:01:55 +0300

    Add mutation to graphql api for insert new computer
    Because now inserting via UI not implemented.
    It can be tested in QraphiQL UI:
    * go to URLL: http://localhost:3000/api/v1;
    * then insert mutation query described above:
    
    ```javascript
    mutation{insertComputer(mark:"SuperBrand", model:"Dab", color:"red", wifi:"802.11ac", isLaptop:true, diagonal:13, coresNumber:42, usb2:2, usb3:3, ram:256, memory:32, videocard:"SuperPuper", videomemory:512, processor:"MegaGerc", operatingSystem:"OS100500", price:100500)
    {
      mark
        model
          color
            wifi
    	  isLaptop
    	    diagonal
    	      coresNumber
    	        usb2
    		  usb3
    		    ram
    		      memory
    		        videocard
    			  videomemory
    			    processor
    			      operatingSystem
    			        price
    				}}
    ```
    * go to computers list and find new inserted.

--- src/server/schema/schema.js
+++ src/server/schema/schema.js
@@ -2,7 +2,11 @@ import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLList,
-  GraphQLString
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLNonNull
 } from 'graphql';
 
 import { ComputerType } from './computerType';
@@ -34,6 +38,96 @@ let schema = new GraphQLSchema({
                                 }
 
                                 resolve(docs);
+                            }
+                        });
+                    });
+                }
+            }
+        }
+    }),
+
+    mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: {
+            insertComputer: {
+                type: ComputerType,
+                args: {
+                    mark: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The mark of computer',
+                    },
+                    model: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The model of computer',
+                    },
+                    color: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The color of computer'
+                    },
+                    wifi: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The type of wifi'
+                    },
+                    isLaptop: {
+                        type: new GraphQLNonNull(GraphQLBoolean),
+                        description: 'The computer is laptop'
+                    },
+                    diagonal: {
+                        type: new GraphQLNonNull(GraphQLFloat),
+                        description: 'The monitor diagonal'
+                    },
+                    coresNumber: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The cpu core count'
+                    },
+                    usb2: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The usb2 ports count'
+                    },
+                    usb3: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The usb3 ports count'
+                    },
+                    ram: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The ram capacity'
+                    },
+                    memory: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The memory capacity'
+                    },
+                    videocard: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The type of video card in computer'
+                    },
+                    videomemory: {
+                        type: new GraphQLNonNull(GraphQLInt),
+                        description: 'The video memory capacity computer'
+                    },
+                    processor: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The type of processor in computer'
+                    },
+                    operatingSystem: {
+                        type: new GraphQLNonNull(GraphQLString),
+                        description: 'The OS on computer'
+                    },
+                    price: {
+                        type: new GraphQLNonNull(GraphQLFloat),
+                        description: 'The price of computer'
+                    }
+                },
+                resolve: (obj, newComputer ) => {
+                    return new Promise((resolve, reject) => { // Also must be moved to separate file, like and query
+                        const newComputerModel = new ComputerModel(newComputer);
+
+                        newComputerModel.save(function(err, docs) {
+                            if(err) {
+                                console.log('error: ', err);
+                                reject({error: err});
+                            }
+                            else {
+                                resolve(newComputerModel);
                             }
                         });
                     });
