b45efb777232d854676c6228787bfe4a3a9d0c82
urmybestfriend <c.goldentiger.c@gmail.com>
2016-10-31 11:56:32 +0300

    Use `generic` like controler for manipulating with db entities (computers/phones/printers)

--- src/server/controllers/printers.js
+++ src/server/controllers/printers.js
@@ -1,39 +1,11 @@
 'use strict';
 
 import PrinterModel from '../models/printers';
+import commonCtrl from './common';
 
-const getAll = (mark) => {
-    return new Promise((resolve, reject) => {
-        PrinterModel.find({}, (err, printers) => {
-            if (!err) {
-                if (mark) {
-                    mark = mark.trim().toLowerCase();
+const getAll = (mark) => commonCtrl.getAll(PrinterModel, mark);
 
-                    printers = printers.filter(p => p.mark.toLowerCase() === mark);
-                }
-
-                resolve(printers);
-            } else {
-                reject({ error: err });
-            }
-        });
-    });
-};
-
-const add = (printer) => {
-    return new Promise((resolve, reject) => {
-        const printerModel = new PrinterModel(printer);
-
-        printerModel.save(function(err, docs) {
-            if(!err) {
-                resolve(printerModel);
-            }
-            else {
-                reject(err);
-            }
-        });
-    });
-};
+const add = (printer) => commonCtrl.add(PrinterModel, printer);
 
 export default {
     getAll,
